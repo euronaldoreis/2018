6588b9d7a4f9f5ff870d4efd791bc886925757f5
euronaldoreis <reeisfst@gmail.com>
2018-04-11 09:34:55 -0400

    update config database

--- application/config/database.php
+++ application/config/database.php
@@ -75,9 +75,9 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
+	'hostname' => 'mysql472.umbler.com',
+	'username' => 'liga_tarcio',
+	'password' => 'r0ck3tm4n',
 	'database' => 'liga_tarcio',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
