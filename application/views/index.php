9a66d3f8cdd61ccb7fb3568c7b3f97ad3fe8005b
euronaldoreis <reeisfst@gmail.com>
2018-04-12 17:41:34 -0400

    Nov atta

--- application/views/index.php
+++ application/views/index.php
@@ -58,10 +58,13 @@
 		.center {
 			text-align: center;
 		}
+		.nav .brand-logo {
+			width: 250px!important;
+		}
 	</style>
   <nav class="nav-extended teal darken-4">
     <div class="nav-wrapper">
-      <a href="#" class="brand-logo center"><img src="assets/img/favicon.png" style="width: 40px; margin-right: 10px;">Liga Tarcio</a>
+      <a href="#" class="brand-logo center"><img src="assets/img/favicon_branco.png" style="width: 40px; margin-right: 10px;">Liga Tarcio</a>
     </div>
     <div class="nav-content center">
       <ul class="tabs tabs-transparent">
